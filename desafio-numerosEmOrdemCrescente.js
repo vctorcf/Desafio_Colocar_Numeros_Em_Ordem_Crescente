970357dbae2dcc18e4d2b319eaa40791573be078
Victor Chagas Franca <vctorcf@gmail.com>
2025-03-12 12:03:12 -0300

    otimizando e corrigindo

--- desafio-numerosEmOrdemCrescente.js
+++ desafio-numerosEmOrdemCrescente.js
@@ -1,23 +1,17 @@
 function testar(){
+    const numeros = [];
+
     let n1 = document.getElementById("n1").value;
+    numeros.push(n1);
     let n2 = document.getElementById("n2").value;
+    numeros.push(n2);
     let n3 = document.getElementById("n3").value;
+    numeros.push(n3);
 
     if (n1==n2||n2==n3||n1==n3||isNaN(n1)||isNaN(n2)||isNaN(n3)){
-        alert("Por favor, digite apenas números que sejam diferentes entre si.");
+        alert("Por favor, digite nos espaços somente números que sejam diferentes entre si.");
     } else {
-        if(n1<n2&&n2<n3){
-            alert(`A ordem crescente é ${n1}, ${n2}, ${n3}.`);
-        } else if(n2<n1&&n1<n3){
-            alert(`A ordem crescente é ${n2}, ${n1}, ${n3}.`);
-        } else if(n2<n3&&n3<n1){
-            alert(`A ordem crescente é ${n2}, ${n3}, ${n1}.`);
-        } else if(n1<n3&&n3<n2){
-            alert(`A ordem crescente é ${n1}, ${n3}, ${n2}.`);
-        } else if(n3<n1&&n1<n2){
-            alert(`A ordem crescente é ${n3}, ${n1}, ${n2}.`);
-        } else if(n3<n2&&n2<n1){
-            alert(`A ordem crescente é ${n3}, ${n2}, ${n1}.`);
-        }
+      numeros.sort(function(a, b){return a - b});
+      alert(`Números digitados em ordem crescente:${numeros}.`);
     }
 }
